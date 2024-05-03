e1a4c386df65505b3a0dd28cebc66e1132467f92
DoberAbo <156190233+DoberAbo@users.noreply.github.com>
2024-05-03 16:25:06 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,37 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var dropdown = document.querySelector('.dropdown_6');
+  
+    dropdown.addEventListener('click', function() {
+      this.classList.toggle('open');
+    });
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    var dropdown = document.querySelector('.dropdown_5');
+  
+    dropdown.addEventListener('click', function() {
+      this.classList.toggle('open');
+    });
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    var dropdown = document.querySelector('.dropdown_4');
+  
+    dropdown.addEventListener('click', function() {
+      this.classList.toggle('open');
+    });
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    var dropdown = document.querySelector('.dropdown_3');
+  
+    dropdown.addEventListener('click', function() {
+      this.classList.toggle('open');
+    });
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    var dropdown = document.querySelector('.dropdown_2');
+  
+    dropdown.addEventListener('click', function() {
+      this.classList.toggle('open');
+    });
+  });
